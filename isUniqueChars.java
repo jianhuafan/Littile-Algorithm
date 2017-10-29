a4a22bd4786f3a2838fa66061c4d9243b524b1bb
Jianhua Fan <cocotalker@163.com>
2017-10-28 21:28:41 -0400

    Add files via upload

---
+++ isUniqueChars.java
@@ -0,0 +1,11 @@
+public static boolean isUniqueChars(String str) {
+	int checker = 0;
+	for(int i = 0; i < str.length(); ++i) {
+		int val = str.charAt(i) - 'a';
+		if (checker & (1 << var) > 0) {
+			return false;
+		}
+		checker |= (1 << var);
+	}
+	return true;
+}
